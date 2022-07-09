0d83cf1a1c880c7fd03d896d79aeec5a2e6ebaa0
ZadAndRot <kasirecivanna@gmail.com>
2022-07-09 22:02:48 +0300

    container deleted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
-import Container from "../container/index";
+import Profile from "../components/Profile";
+import Statistics from "../components/Statistics/index";
+import FriendList from "../components/FriendsList/index";
+import TransactionHistory from "../components/TransactionHistory";
+import user from "../../src/user.json"
+import data from "../../src/data.json"
+import friends from "../../src/friends.json"
+import transactions from "../../src/transactions.json"
 
 export const App = () => {
   return (
@@ -12,11 +19,31 @@ export const App = () => {
         color: '#010101',
         backgroundColor:'lightgray',
         position: "absolute",
-        width: "100vw"
+        width: "100vw",
+        flexDirection:"column",
+        padding: "100px",
+        boxSizing:"border-box"
       }}
     >
     
-    <Container/>
+    <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+        />
+        <Statistics 
+            title="Upload stats" 
+            stats={data} 
+        />
+        <Statistics stats={data} />
+        <FriendList
+            friends={friends}
+        />
+        <TransactionHistory 
+            items={transactions}
+        />
 
     
     
